177e61815da29191f30a686e826df8854115bf9c
mr.lei <17685219818@163.com>
2024-11-18 23:21:40 +0800

    Initial commit

---
+++ src/test/java/com/example/normaltest/NormalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.normaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
